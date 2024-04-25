317136708828f08bd547708759b4ebf96ea5ccc6
YoberMarlow29 <yober2828@gmail.com>
2024-04-24 21:29:49 -0300

    sprint1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { QuienSoyComponent } from './componentes/quien-soy/quien-soy.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,QuienSoyComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { QuienSoyComponent } from './componentes/quien-soy/quien-soy.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path:'quien-soy', component:QuienSoyComponent}
+];
