8d6f44c295d1c2357a2966573a61586ad47e4f40
YoberMarlow29 <yober2828@gmail.com>
2024-04-25 10:40:31 -0300

    cambios de pequeñas cosas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { QuienSoyComponent } from './componentes/quien-soy/quien-soy.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,QuienSoyComponent],
+  imports: [RouterOutlet,QuienSoyComponent,LoginComponent,HomeComponent,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,12 @@
 import { Routes } from '@angular/router';
 import { QuienSoyComponent } from './componentes/quien-soy/quien-soy.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { HomeComponent } from './componentes/home/home.component';
 
 export const routes: Routes = [
 
-    {path:'quien-soy', component:QuienSoyComponent}
+    {path:'login',component:LoginComponent},
+    {path:'home',component:HomeComponent},
+    {path:'quien-soy',component:QuienSoyComponent},
+    { path: '', redirectTo: 'quien-soy', pathMatch: 'full' }    
 ];
