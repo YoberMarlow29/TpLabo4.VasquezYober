979e6fd6e2bf64ed0abb31b502f5c127632964ec
YoberMarlow29 <yober2828@gmail.com>
2024-05-01 10:03:37 -0300

    configuracion firebase agregada

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,12 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"tplab-4","appId":"1:737067449284:web:3c639aa0fdc8c964700291","storageBucket":"tplab-4.appspot.com","apiKey":"AIzaSyD7t9oaqshDxd81OVRXpz_v-AmF9Gw2m3Y","authDomain":"tplab-4.firebaseapp.com","messagingSenderId":"737067449284"}))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideFirestore(() => getFirestore())), importProvidersFrom(provideStorage(() => getStorage()))]
 };
